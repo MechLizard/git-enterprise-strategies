07d20776a8199e1b93f172610da2c4f6845143b5
Izzy Jones <jones.r1@ufl.edu>
2024-09-08 10:47:12 -0400

    Merged text

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import gatorLogo from './gator.gif';
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={gatorLogo} className="App-logo" alt="logo" />
-        <p> Hello World! </p>
-        <p> My name is Izzy and this is my project </p>
-      </header>
+        <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo"/>
+            <p> Hello World! </p>
+            <p> My name is Izzy and this is my project </p>
+            <p>
+                Hello World! This is our team project.
+            </p>
+        </header>
     </div>
   );
 }
